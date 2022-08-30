1b7e703adfbd5ab30f962237a57a6ec008a8b0e8
tolmany <jollydev0821@gmail.com>
2022-08-30 11:29:16 +0900

    Website meta image deleted

--- src/components/Meta/Meta.js
+++ src/components/Meta/Meta.js
@@ -14,7 +14,6 @@ export const Meta = ({ title, description, prefix = name, ogImage = defaultOgIma
       <meta key="description" name="description" content={description} />
       <meta name="author" content={name} />
 
-      <meta property="og:image" content={ogImage} />
       <meta property="og:image:alt" content="Banner for the site" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1280" />
